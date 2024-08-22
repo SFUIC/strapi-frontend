c23dcdbdd4f36db04ffbf3441c938a98419941e6
Builderbot2000 <skenshin2000@outlook.com>
2024-08-21 20:14:12 -0700

    remote image permission added for cloud

--- next.config.js
+++ next.config.js
@@ -16,6 +16,16 @@ const nextConfig = {
         protocol: "https",
         hostname: "images.pexels.com",
       },
+      {
+        protocol: "https",
+        hostname: process.env.NEXT_PUBLIC_STRAPI_API_URL,
+        pathname: "/uploads/**", // Strapi backend uploads path
+      },
+      {
+        protocol: "https",
+        hostname: process.env.NEXT_PUBLIC_S3_BUCKET_URL,
+        pathname: "/**", // S3 bucket path
+      },
     ],
   },
 };
